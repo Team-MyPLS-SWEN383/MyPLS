9a44192bddf52c35240908c691e5e89feefd5fa2
Kenny S <35939549+kxs6629@users.noreply.github.com>
2021-03-02 01:27:43 -0500

    Completed implementation of database connectivity. Able to demonstrate query that returns info based on role

--- src/DBLayer.java
+++ src/DBLayer.java
@@ -1,6 +1,7 @@
 import java.sql.*;
 
 public class DBLayer {
+    // Class attributes
     private final String URI;
     private final String DRIVER;
     private final String USER;
@@ -10,13 +11,20 @@ public class DBLayer {
     private ResultSet rs;
     private String sql;
 
+    /**
+     * Constructor for DBLayer
+     */
     public DBLayer(){
-        URI = "jdbc:mysql://localhost/project_db?autoReconnect=true&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        URI = "jdbc:mysql://localhost/mypls?autoReconnect=true&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
         DRIVER = "com.mysql.cj.jdbc.Driver";
         USER = "root";
         PASSWORD = "AyyLmao12";
     }
 
+    /**
+     * Connect to MySQL database using credentials from constructor
+     * @return true if connected, false if an error occurs
+     */
     public boolean connect(){
         conn = null;
         try  {
@@ -36,6 +44,10 @@ public class DBLayer {
         }// end of catch
     }
 
+    /**
+     * Disconnect from MySQL database
+     * @return true if successful, false if an error occurs
+     */
     public boolean close() {
         try {
             conn.close();
@@ -46,25 +58,43 @@ public class DBLayer {
         }//end of catch
     }// end of method close
 
-    public void getUser(String _user){
+    /**
+     * Get role of user and print it out to the console
+     * @param _user username to be used in query
+     */
+    public void getUserRole(String _user){
         try{
             s = conn.createStatement();
-            sql = "";
+            sql = "SELECT role_name FROM roles WHERE idRoles IN (SELECT Roles_idRoles FROM user WHERE username = '"+_user+"')";
             rs = s.executeQuery(sql);
             if(rs.next()){
-                System.out.println();
+                System.out.println("Name: "+_user+ " Role: "+rs.getString(1));
             }
             else{
                 System.out.println("No results found for: "+_user);
             }
         } catch(SQLException sqle){
-            System.out.println("An SQL Error has occured");
+            System.out.println("An SQL Error has occurred");
+            sqle.printStackTrace();
         }
     }
 
     public static void main(String[] args){
         DBLayer test = new DBLayer();
-        System.out.println(test.connect());
+        // Connect to DB
+        test.connect();
+
+        // Demo prints
+        System.out.println("Test queries for roles");
+        System.out.println("======================");
+        test.getUserRole("prof123");
+        test.getUserRole("stu123");
+        test.getUserRole("manager");
+        System.out.println("Next one should fail");
+        test.getUserRole("AAAAAAA");
+
+        // Disconnect from DB
+        test.close();
     }
 
 }
